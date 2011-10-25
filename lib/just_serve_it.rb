1de7031402b3fedc147b325b12fadbbfd3bd89ec
Ryan Cook <cookrn@gmail.com>
2011-10-24 23:06:58 -0600

    add basic env support

--- lib/just_serve_it.rb
+++ lib/just_serve_it.rb
@@ -1,10 +1,18 @@
+require "active_support/core_ext/module/attribute_accessors"
 require "just_serve_it/ext/kernel"
 require "just_serve_it/app_builder"
 
 module JustServeIt
+  mattr_accessor :env
+  @@env = "development"
+
   def self.serve( *resources )
     app_builder = JustServeIt::AppBuilder.new *resources
     app_builder.rack_app
+  end
+
+  def self.setup
+    yield self
   end
 end
 
